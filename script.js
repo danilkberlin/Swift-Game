286ac9a1d3f132dbb087a6445e251d4e0a60fdca
danilkberlin <danilkravets19@gmail.com>
2024-03-23 01:17:27 +0100

    update

---
+++ script.js
@@ -0,0 +1,77 @@
+const allLanguage = [en, de, ua];
+const langArr = {  
+    // =================== Navbar ================ 
+    'about': {
+        'en' : 'About',
+        'de' : 'Über',
+        'ua' : 'Про Компанию'
+    },
+    'opening-times': {
+        'en' : 'Opening Time',
+        'de' : 'Öffnungszeit',
+        'ua' : 'Часи роботи'
+    },
+    'price' : {
+        'en' : 'Price',
+        'de' : 'Preis',
+        'ua' : 'Прайс'
+    },
+    'tournaments' : {
+        'en' : 'Tournaments',
+        'de' : 'Turniere',
+        'ua' : 'Турніри'
+    },
+    'gallery' : {
+        'en' : 'Gallery',
+        'de' : 'Galerie',
+        'ua' : 'Галерея',
+    },
+    'contacts': {
+        'en' : 'Contacts',
+        'de' : 'Kontakte',
+        'ua' : 'Контакти'
+    },
+    //============================================
+    //================= About ====================
+    'about-swift-gaming' : {
+        'en' : 'About Swift Gaming',
+        'de' : 'Über Swift Gaming',
+        'ua' : 'Про Swift Gaming'
+    },
+    'text-about' : {
+        'en' : 'We welcome you to the first and only computer club where a friendly atmosphere merges with intense competition, allowing every player to realize his or her potential. We have a place not only for amateurs, but also for those who want to start conquering the world of cyber sports. There is no time to think - act fast, show your skills and win glory!',
+        'de' : 'Wir begrüßen Sie im ersten und einzigen Computerclub, in dem eine freundliche Atmosphäre mit intensivem Wettbewerb verschmilzt und es jedem Spieler ermöglicht, sein Potenzial auszuschöpfen. Wir haben nicht nur einen Platz für Amateure, sondern auch für diejenigen, die die Welt des Cybersports erobern möchten. Es bleibt keine Zeit zum Nachdenken – handeln Sie schnell, zeigen Sie Ihr Können und gewinnen Sie Ruhm!',
+        'ua' : `Ми запрошуємо вас у перший і єдиний комп'ютерний клуб, де дружня атмосфера поєднується з інтенсивним змаганням, дозволяючи кожному гравцеві реалізувати свій потенціал. У нас є місце не лише для аматорів, а й для тих, хто хоче почати завойовувати світ кіберспорту. Немає часу думати - дійте швидко, покажіть свої навички й здобудьте славу!`
+    },
+    //============================================
+    //============== Opening Time =====================
+    'opening-times-in-container': {
+        'en' : 'Opening Time',
+        'de' : 'Öffnungszeit',
+        'ua' : 'Часи роботи'
+    },
+    'monday' : {
+        'en' : 'Monday: Closed',
+        'de' : 'Montag: geschlossen',
+        'ua' : 'Понеділок Зачинено'
+    },
+    'tuesday-thursday' : {
+        'en' : 'Tuesday – Thursday : 12:00-4:00',
+        'de' : 'Dienstag – Donnerstag: 12:00–16:00',
+        'ua' : 'Вівторок - Четверг :12:00-16:00'
+    },
+    'tuesday-thursday' : {
+        'en' : 'Friday – Sunday: 12:00-6:00',
+        'de' : 'Freitag – Sonntag: 12:00–18:00',
+        'ua' : ''
+    },
+
+
+    // //==================== For All ========================
+    'know-more' : {
+        'en' : 'Know more',
+        'de' : 'Erfahren Sie mehr',
+        'ua' : 'Дізнатися більше'
+    }
+
+}
